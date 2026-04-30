612b8705e7df5a6de0420fe045dcc01f1422ade4
DuskFlare13 <Redstoner113@gmail.com>
2026-04-30 18:31:27 -0300

    wawa

---
+++ LantasChainCrafting/Logic.cs
@@ -0,0 +1,190 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using static MainMenuLoadButton;
+
+namespace ChainCrafting
+{
+    public record Resource(TechType Type, int Amount)
+    {
+        public int Yield => TechData.GetCraftAmount(Type);
+    }
+
+    public static class CraftingLogic
+    {
+        public static CraftTree.Type craftingTree;
+        public static IEnumerator Craft(Crafter crafter, TechType techType)
+        {
+            ChainCraft(techType, Inventory.main, out Stack<Resource> craftStack);
+
+            while (craftStack.Any())
+            {
+                Resource item = craftStack.Pop();
+                TechType next = item.Type;
+                for (int i = 0; i < item.Amount; i++)
+                {
+                    TechData.GetCraftTime(next, out float craftTime);
+                    craftTime = Math.Max(craftTime, 2.7f);
+                    if (!Consume(next)) continue;
+                    crafter._logic.Craft(next, craftTime);
+                    while (crafter.HasCraftedItem()) yield return null;
+                }
+            }
+        }
+
+        public static void IsFuffiled(TechType techType, Inventory inventory, ref bool alreadyPassed)
+        {
+            Plugin.Logger.LogInfo($"Checking if crafting {techType} is fulfilled.");
+            if (alreadyPassed) return;
+            ChainCraft(techType, inventory, out Stack<Resource> craftStack);
+            CostOfCraft(craftStack, out Dictionary<TechType, int> entryCost);
+            ValidateCraft(inventory, entryCost, out bool result);
+            alreadyPassed = result;
+        }
+
+
+        public static void ChainCraft(TechType resource, Inventory inventory, out Stack<Resource> craftStack)
+        {
+            craftStack = new Stack<Resource>();
+            CreateStack(resource, ref craftStack, 1);
+            OrganizeCraftStack(ref craftStack);
+            RemoveOwned(ref craftStack, inventory);
+        }
+
+
+        public static void GetRequirements(Resource resource, out Stack<Resource> stack)
+        {
+            stack = new Stack<Resource>();
+            CreateStack(resource.Type, ref stack, resource.Amount);
+            OrganizeCraftStack(ref stack);
+        }
+
+        public static void CreateStack(TechType recipe, ref Stack<Resource> stack, int amount)
+        {
+            if (recipe == TechType.None) return;
+            if (!CraftTree.IsCraftable(recipe) || IsInCraftingTree(recipe)) return;
+            stack.Push(new Resource(recipe, amount));
+            ReadOnlyCollection<Ingredient> component = TechData.GetIngredients(recipe);
+            foreach (Ingredient ingredient in component) CreateStack(ingredient.techType, ref stack, ingredient.amount * amount);
+        }
+
+        public static void OrganizeCraftStack(ref Stack<Resource> craftStack)
+        {
+            Dictionary<TechType, int> catalog = new();
+            Stack<Resource> tempStack = new();
+            while (craftStack.Any())
+            {
+                Resource resource = craftStack.Pop();
+                if (!catalog.ContainsKey(resource.Type))
+                {
+                    catalog.Add(resource.Type, resource.Amount);
+                    tempStack.Push(resource);
+                }
+                else catalog[resource.Type] += resource.Amount;
+            }
+            while (tempStack.Any())
+            {
+                TechType resource = tempStack.Pop().Type;
+                craftStack.Push(new Resource(resource, catalog[resource]));
+            }
+        }
+
+        public static void RemoveOwned(ref Stack<Resource> craftStack, Inventory inventory)
+        {
+            Dictionary<TechType, int> catalog = new();
+            Stack<Resource> tempStack = new();
+            TechType target = craftStack.Last().Type;
+            while (craftStack.Any())
+            {
+                Resource item = craftStack.Pop();
+                int owned = inventory.GetPickupCount(item.Type);
+                int removedCount = Math.Min(item.Amount, owned);
+                int itemYield = item.Yield;
+                catalog[item.Type] = (int)Math.Ceiling((float)Math.Max(0, item.Amount - removedCount) / itemYield);
+                GetRequirements(item, out Stack<Resource> requirements);
+                if (item.Type != target && removedCount > 0)
+                {
+                    while (requirements.Any())
+                    {
+                        Resource requirement = requirements.Pop();
+                        TechType type = requirement.Type;
+                        if (catalog.ContainsKey(type)) catalog[type] = (int)Math.Ceiling((float)Math.Max(0, requirement.Amount - removedCount) / requirement.Yield);
+                    }
+                }
+                tempStack.Push(item);
+            }
+            while (tempStack.Any())
+            {
+                TechType item = tempStack.Pop().Type;
+                craftStack.Push(new Resource(item, catalog[item]));
+            }
+        }
+
+        public static void CostOfCraft(Stack<Resource> craftStack, out Dictionary<TechType, int> entryCost)
+        {
+            entryCost = new();
+            foreach (Resource resource in craftStack)
+            {
+                int materialCount = resource.Amount;
+                if (materialCount <= 0) continue;
+
+                ReadOnlyCollection<Ingredient> ingredients = TechData.GetIngredients(resource.Type);
+                foreach (Ingredient ingredient in ingredients)
+                {
+                    TechType type = ingredient.techType;
+                    if (CraftTree.IsCraftable(type)) continue;
+                    int amount = ingredient.amount * materialCount;
+                    if (entryCost.ContainsKey(type)) entryCost[type] += amount;
+                    else entryCost[type] = amount;
+                }
+            }
+        }
+
+        public static bool Consume(TechType techType)
+        {
+            if (CrafterLogic.IsCraftRecipeFulfilled(techType))
+            {
+                Inventory.main.ConsumeResourcesForRecipe(techType, null);
+                return true;
+            }
+            ErrorMessage.AddWarning(Language.main.Get("DontHaveNeededIngredients"));
+            return false;
+        }
+
+        public static bool IsInCraftingTree(TechType techType)
+        {
+            if (craftingTree is CraftTree.Type.None) return false;
+            CraftTree tree = CraftTree.GetTree(craftingTree);
+            if (tree?.nodes == null) return false;
+            foreach (CraftNode node in tree.nodes)
+            {
+                if (node.techType0 == techType && CraftTree.IsCraftable(techType)) return true;
+            }
+            return false;
+        }
+
+        private static void ValidateCraft(Inventory inventory, Dictionary<TechType, int> entryCost, out bool valid)
+        {
+            Plugin.Logger.LogInfo("Validating craft requirements.");
+            foreach (TechType material in entryCost.Keys)
+            {
+                Plugin.Logger.LogInfo($"Checking material: {material}. Required: {entryCost[material]}, Owned: {inventory.GetPickupCount(material)}");
+                if (entryCost[material] > inventory.GetPickupCount(material))
+                {
+                    Plugin.Logger.LogInfo($"Player does not have enough {material} to craft the target item. Required: {entryCost[material]}, Owned: {inventory.GetPickupCount(material)}");
+                    valid = false;
+                    return;
+                }
+            }
+            valid = true;
+            return;
+        }
+    }
+}
--- LantasChainCrafting/Plugin.cs
+++ LantasChainCrafting/Plugin.cs
@@ -2,13 +2,14 @@
 using BepInEx.Logging;
 using HarmonyLib;
 using System;
+using System.CodeDom.Compiler;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection;
-using UnityEngine;
 
-namespace LantasChainCrafting
+namespace ChainCrafting
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
@@ -19,10 +20,9 @@ namespace LantasChainCrafting
 
         private void Awake()
         {
-            // plugin startup logic
             Logger = base.Logger;
 
-            // register harmony patches, if there are any
+
             Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
@@ -30,31 +30,122 @@ namespace LantasChainCrafting
 
     }
 
-    [HarmonyPatch(typeof(Crafter), nameof(Crafter.Craft))]
-    public class ChainCrafter
+    [HarmonyPatch]
+    public static class ChainCrafting
     {
-        [HarmonyPrefix]
-        public static bool Prefix(Crafter __instance, TechType techType, float duration)
+        [HarmonyPatch(typeof(CrafterLogic))]
+        [HarmonyPatch(nameof(CrafterLogic.IsCraftRecipeFulfilled))]
+        [HarmonyPostfix]
+        public static void Validate(TechType techType, ref bool __result)
         {
-            ChainCrafter chain = new();
-            return chain.OnCraft(__instance, techType, duration);
+            CraftingLogic.IsFuffiled(techType, Inventory.main, ref __result);
         }
-        public bool OnCraft(Crafter crafter, TechType techType, float duration)
+
+        [HarmonyPatch(typeof(CrafterLogic))]
+        [HarmonyPatch(nameof(CrafterLogic.ConsumeResources))]
+        [HarmonyPrefix]
+        public static bool ConsumePrefix(ref bool __result)
         {
-            Stack<TechType> craftStack = new();
-            ChainCraft(techType, craftStack);
-            if (!validateRequirement(craftStack, techType)) return false;
+            __result = true;
+            return false;
+        }
+
+        [HarmonyPatch(typeof(Crafter))]
+        [HarmonyPatch(nameof(Crafter.Craft))]
+        [HarmonyPrefix]
+        public static bool Prefix(Crafter __instance, TechType techType)
+        {
+            if (GameModeUtils.RequiresIngredients()) __instance.StartCoroutine(CraftingLogic.Craft(__instance, techType));
+            else return true;
+            return false;
+        }
+
+        [HarmonyPatch(typeof(uGUI_CraftingMenu))]
+        [HarmonyPatch(nameof(uGUI_CraftingMenu.Open))]
+        [HarmonyPrefix]
+        public static bool Open(CraftTree.Type treeType)
+        {
+            Plugin.Logger.LogInfo("Loading CraftingTree Type.");
+            CraftingLogic.craftingTree = treeType;
+            Plugin.Logger.LogInfo($"CraftingTree Type: {CraftingLogic.craftingTree}");
+            return true;
+        }
+    }
+
+    /*public static class ChainCrafter
+    {
+        [HarmonyPatch(typeof(CrafterLogic))]
+        [HarmonyPatch(nameof(CrafterLogic.IsCraftRecipeFulfilled))]
+        [HarmonyPostfix]
+        public static void Postfix(TechType techType, ref bool __result)
+        {
+            IsFuffiled(techType, ref __result);
+        }
+
+        [HarmonyPatch(typeof(CrafterLogic))]
+        [HarmonyPatch(nameof(CrafterLogic.ConsumeResources))]
+        [HarmonyPrefix]
+        public static bool ConsumePrefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+
+        [HarmonyPatch(typeof(Crafter))]
+        [HarmonyPatch(nameof(Crafter.Craft))]
+        [HarmonyPrefix]
+        public static bool Prefix(Crafter __instance, TechType techType)
+        {
+            __instance.StartCoroutine(Craft(__instance, techType));
+            return false;
+        }
+
+        public static void IsFuffiled(TechType techType, ref bool alreadyPassed)
+        {
+            if (alreadyPassed) return;
+            Inventory inventory = Inventory.main;
+            ChainCraft(techType, out _, out Dictionary<TechType, int> entry);
+            CostOfCraft(entry, out Dictionary<TechType, int> entryCost);
+            ValidateRequirement(inventory, entryCost, out alreadyPassed);
+        }
+
+        private static void ChainCraft(TechType techType, out Stack<TechType> craftStack, out Dictionary<TechType, int> entry)
+        {
+            Inventory inventory = Inventory.main;
+            Stack<TechType> tempStack = new();
+            CrudeStackCraft(techType, ref tempStack);
+            OrganizeCraftStack(ref tempStack, out entry, out craftStack);
+            RemoveOwned(inventory, ref craftStack, ref entry);
+        }
+
+        public static bool Consume(TechType techType)
+        {
+            if (CrafterLogic.IsCraftRecipeFulfilled(techType))
+            {
+                Inventory.main.ConsumeResourcesForRecipe(techType, null);
+                return true;
+            }
+            ErrorMessage.AddWarning(Language.main.Get("DontHaveNeededIngredients"));
+            return false;
+        }
+
+        public static IEnumerator Craft(Crafter crafter, TechType techType)
+        {
+            ChainCraft(techType, out Stack<TechType> craftStack, out _);
 
             while (craftStack.Any())
             {
                 TechType next = craftStack.Pop();
-                TechData.GetCraftTime(next, out duration);
-                if (next != techType) crafter.Craft(next, duration);
+                TechData.GetCraftTime(next, out float craftTime);
+
+                craftTime = Math.Max(craftTime, 2.7f);
+                if (!Consume(next)) continue;
+                crafter.logic.Craft(next, craftTime);
+                while (crafter.HasCraftedItem()) yield return null;
             }
-            return true;
         }
 
-        private void ChainCraft(TechType techType, Stack<TechType> craftStack)
+        private static void CrudeStackCraft(TechType techType, ref Stack<TechType> craftStack)
         {
             if (!CraftTree.IsCraftable(techType)) return;
             craftStack.Push(techType);
@@ -64,53 +155,70 @@ namespace LantasChainCrafting
                 TechType type = ingredient.techType;
                 if (!CraftTree.IsCraftable(type)) continue;
                 int amount = ingredient.amount;
-                for (int i = 0; i < amount; i++) ChainCraft(type, craftStack);
+                for (int i = 0; i < amount; i++) CrudeStackCraft(type, ref craftStack);
             }
         }
 
-        private bool validateRequirement(Stack<TechType> craftStack, TechType target)
+        private static void OrganizeCraftStack(ref Stack<TechType> craftStack, out Dictionary<TechType, int> entry, out Stack<TechType> uniqueStack)
         {
-            Inventory main = Inventory.main;
             Stack<TechType> temp = new();
-            Stack<TechType> toRemove = new();
-            Dictionary<TechType, int> entry = new();
-            Dictionary<TechType, int> entryCost = new();
+            entry = new();
             while (craftStack.Any())
             {
-               TechType nextRecipe = craftStack.Pop();
-                if (entry.ContainsKey(nextRecipe)) entry[nextRecipe] += 1;
-                else 
+                TechType item = craftStack.Pop();
+                if(entry.ContainsKey(item)) entry[item] += 1;
+                else
                 {
-                    temp.Push(nextRecipe); 
-                    entry[nextRecipe] = 1; 
+                    temp.Push(item);
+                    entry[item] = 1;
                 }
             }
+            uniqueStack = new Stack<TechType>(temp);
+            while (temp.Any())
+            {
+                TechType item = temp.Pop();
+                for (int i = 0; i < entry[item]; i++) craftStack.Push(item);
+            }
+        }
+
+        private static void RemoveOwned(Inventory inventory, ref Stack<TechType> craftStack, ref Dictionary<TechType, int> entry)
+        {
+            TechType target = craftStack.Last();
+            Stack<TechType> temp = new(craftStack);
+            Stack<TechType> toRemove = new();
+            craftStack.Clear();
             while (temp.Any())
             {
                 TechType item = temp.Pop();
                 int itemCount = entry[item];
                 int itemYield = TechData.GetCraftAmount(item);
-                int playerOwned = main.GetPickupCount(item);
+                int playerOwned = inventory.GetPickupCount(item);
                 int removedCount = Math.Min(itemCount, playerOwned);
-                if(item != target && removedCount > 0)
+                if (item != target && removedCount > 0)
                 {
                     entry[item] = (int)Math.Ceiling((float)Math.Max(0, itemCount - removedCount) / itemYield);
-                    ChainCraft(item, toRemove);
+                    CrudeStackCraft(item, ref toRemove);
+                    OrganizeCraftStack(ref toRemove, out _, out _);
                     while (toRemove.Any())
                     {
                         TechType nextRecipe = toRemove.Pop();
                         if (nextRecipe == item) continue;
                         int count = entry[nextRecipe];
                         int recipeYield = TechData.GetCraftAmount(nextRecipe);
-                        if (entry.ContainsKey(nextRecipe)) entry[nextRecipe] = (int)Math.Ceiling((float)Math.Max(0, count - removedCount)/recipeYield);
+                        if (entry.ContainsKey(nextRecipe)) entry[nextRecipe] = (int)Math.Ceiling((float)Math.Max(0, count - removedCount) / recipeYield);
                     }
                 }
                 for (int i = 0; i < entry[item]; i++)
-                { 
-                    craftStack.Push(item); 
+                {
+                    craftStack.Push(item);
                 }
             }
-            foreach(TechType material in entry.Keys)
+        }
+
+        public static void CostOfCraft(Dictionary<TechType, int> entry, out Dictionary<TechType, int> entryCost)
+        {
+            entryCost = new();
+            foreach (TechType material in entry.Keys)
             {
                 int materialCount = entry[material];
                 if (materialCount <= 0) continue;
@@ -125,11 +233,22 @@ namespace LantasChainCrafting
                     else entryCost[type] = amount;
                 }
             }
+        }
+
+        private static void ValidateRequirement(Inventory inventory, Dictionary<TechType, int> entryCost, out bool valid)
+        {
             foreach (TechType material in entryCost.Keys)
             {
-                if (entryCost[material] > main.GetPickupCount(material)) return false;
+                if (entryCost[material] > inventory.GetPickupCount(material))
+                {
+                    valid = false;
+                    return;
+                }
             }
-            return true;
+            valid = true;
+            return;
         }
-    }
+
+
+    }*/
 }
